95a4e0184208de8008f42fda7f16b1993400f654
lhas <luizhrqas@gmail.com>
2017-09-30 11:53:40 -0300

    init structure

---
+++ app/screens/Home.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default class Home extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Home</Text>
+        <Text>abacate</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
